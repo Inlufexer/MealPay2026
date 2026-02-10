72377be9dce9474935d70fcadc70b7751e120d08
Theodor Faraon <139110287+Inlufexer@users.noreply.github.com>
2026-02-10 12:06:58 -0800

    Initialized Transaction

--- Transaction.java
+++ Transaction.java
@@ -2,5 +2,10 @@ public class Transaction {
     private int month;
     private int day;
     private double amount;
-    
+
+    public Transaction(int myMonth, int myDay, double myAmount){
+        month = myMonth;
+        day = myDay;
+        amount = myAmount;
+    }
 }
