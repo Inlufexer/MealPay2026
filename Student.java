691a40a7838ca80fe7cec09c0ad7852966e3750a
Anthony Iek <anthonyiek2010@gmail.com>
2026-02-10 12:19:07 -0800

    Creating the student that takes the name, sets balance to 0, and sets it to a unique idNo

--- Student.java
+++ Student.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 public class Student {
     private String name;
     private int idNo;
     private double balance;
-    
+
     private static int idGen = 1;
     
+    private ArrayList<Transaction> transactions;
+
+    // Creating the student
+    public Student(String myName){
+        name = myName;
+        balance = 0;
+        idNo = idGen;
+        idGen++;
+        transactions = new ArrayList<Transaction>();
+    }
 }
