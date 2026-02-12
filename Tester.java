5bbd4b92b652dfa35d085ae12243eb0bdbfde093
Theodor Faraon <139110287+Inlufexer@users.noreply.github.com>
2026-02-12 13:52:46 -0800

    Mistake, anthony fix

--- Tester.java
+++ Tester.java
@@ -1,13 +1,4 @@
 public class Tester {
     public static void main(String[] args){
-        System.out.println("Welcome to MealPay Program\n");
-        System.out.println("Adding students to the program...");
-
-        System.out.println("Displaying Students...");
-        PaySystem paySystem = new PaySystem();
-        paySystem.addStudent("A");
-        paySystem.addStudent("B");
-        paySystem.addStudent("C");
-        paySystem.addStudent("D");
     }
 }
