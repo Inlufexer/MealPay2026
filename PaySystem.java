9932eb19223bb8320c43244d1c5e00b68205b5f0
Anthony Iek <anthonyiek2010@gmail.com>
2026-02-12 13:45:53 -0800

    Created Public PaySystem(). This allows for us to intialize the array of students.

--- PaySystem.java
+++ PaySystem.java
@@ -3,67 +3,76 @@ import java.util.ArrayList;
 public class PaySystem {
 
     private ArrayList<Student> students;
-        
-    public void addStudent(String name){
-        students.add(new Student(name)); //create a new student in student class
+
+    public PaySystem() {
+        students = new ArrayList<Student>();
+    }
+
+    public void addStudent(String name) {
+        students.add(new Student(name)); // create a new student in student class
     }
 
     public void addFunds(int idNo, int month, int day, double amount) {
-        for (int i = 0; i < students.size(); i++){
-            Student s = students.get(i); //i value where i is looping through
-            if (s.getIdNo() == idNo){ //if id searching for is in the students list
-                s.addFunds(month, day, amount); //new transaction
+        for (int i = 0; i < students.size(); i++) {
+            Student s = students.get(i); // i value where i is looping through
+            if (s.getIdNo() == idNo) { // if id searching for is in the students list
+                s.addFunds(month, day, amount); // new transaction
             }
         }
-        System.out.println("Student ID number not found."); //if s.getIdNo does not match with the searching id there is no profile
+        System.out.println("Student ID number not found."); // if s.getIdNo does not match with the searching id there
+                                                            // is no profile
 
     }
 
-    public void chargeLunch(int idNo, int month, int day){
-        for (int i = 0; i < students.size(); i++){
-            Student s = students.get(i); //i value where i is looping through
-            if (s.getIdNo() == idNo){
-                s.chargeLunch(month, day);  //uses Students charge lunch method to subtract $7
+    public void chargeLunch(int idNo, int month, int day) {
+        for (int i = 0; i < students.size(); i++) {
+            Student s = students.get(i); // i value where i is looping through
+            if (s.getIdNo() == idNo) {
+                s.chargeLunch(month, day); // uses Students charge lunch method to subtract $7
             }
         }
 
     }
 
-    public void showNegativeBalance(){
-        for (int i = 0; i < students.size(); i++){
+    public void showNegativeBalance() {
+        for (int i = 0; i < students.size(); i++) {
             Student s = students.get(i);
-                if (s.getBalance() < 0){ // if balance is less than 0 it prints the student
-                    s.toString();
-                }
+            if (s.getBalance() < 0) { // if balance is less than 0 it prints the student
+                s.toString();
+            }
         }
     }
 
-    public void TransactionByDay(int month, int day){
-        for (int i = 0; i < students.size(); i++){
-            Student s = students.get(i); //keeps track of what student we are on/goes through students
+    public void TransactionByDay(int month, int day) {
+        for (int i = 0; i < students.size(); i++) {
+            Student s = students.get(i); // keeps track of what student we are on/goes through students
 
-            ArrayList<Transaction> thatDay = s.getTransactions(); //goes through that students transactions
+            ArrayList<Transaction> thatDay = s.getTransactions(); // goes through that students transactions
 
-            for (int j = 0; j < thatDay.size(); j++){
+            for (int j = 0; j < thatDay.size(); j++) {
                 Transaction t = thatDay.get(j);
 
-                if (t.getDate().equals((month + "/" + day))){ //if the dates are the same
+                if (t.getDate().equals((month + "/" + day))) { // if the dates are the same
                     System.out.println(t + " by " + s.getIdNo());
                 }
             }
-            System.out.println("There were no transactions on " + month + "/" + day); //if the date and month don't add up
+            System.out.println("There were no transactions on " + month + "/" + day); // if the date and month don't add
+                                                                                      // up
         }
 
     }
 
-    public void displayStudent(int idNo){
-        for (int i = 0; i < students.size(); i++){
+    public void displayStudentID(int idNo) {
+        for (int i = 0; i < students.size(); i++) {
             Student s = students.get(i);
-            if (s.getIdNo() == idNo){
-                s.toString(); //using the toString()
+            if (s.getIdNo() == idNo) {
+                s.toString(); // using the toString()
 
             }
         }
-        System.out.println("Student ID number not found."); //if s.getIdNo does not match with the searching id there is no profile
+        System.out.println("Student ID number not found."); // if s.getIdNo does not match with the searching id there
+                                                            // is no profile
     }
+
+    
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,18 @@
 public class Tester {
     public static void main(String[] args){
-        
+        System.out.println("Welcome to MealPay Program\n");
+        System.out.println("Adding students to the program...");
+
+        System.out.println("Displaying Students...");
+        PaySystem paySystem = new PaySystem();
+        paySystem.addStudent("A");
+        paySystem.addStudent("B");
+        paySystem.addStudent("C");
+        paySystem.addStudent("D");
+
+        paySystem.displayStudentID(1);
+        paySystem.displayStudentID(2);
+        paySystem.displayStudentID(3);
+        paySystem.displayStudentID(4);
     }
 }
