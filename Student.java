9f1c1cd9f261631ff275c62236cc845d86debc9f
Anthony Iek <anthonyiek2010@gmail.com>
2026-02-12 13:59:03 -0800

    Print out Students and fixed toString()

--- Student.java
+++ Student.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Student {
-    
+
     private String name;
     private int idNo;
     private double balance;
@@ -10,7 +10,7 @@ public class Student {
     private ArrayList<Transaction> transactions;
 
     // Creating the student
-    public Student(String myName){
+    public Student(String myName) {
         name = myName;
         balance = 0;
         idNo = idGen;
@@ -18,44 +18,44 @@ public class Student {
         transactions = new ArrayList<Transaction>();
     }
 
-    //getter for name
-    public String getName(){
+    // getter for name
+    public String getName() {
         return name;
     }
 
-    //getter for idNo
-    public int getIdNo(){
+    // getter for idNo
+    public int getIdNo() {
         return idNo;
     }
 
-    //getter for balance
-    public double getBalance(){
+    // getter for balance
+    public double getBalance() {
         return balance;
     }
 
-    //getter for transactions
-    public ArrayList<Transaction> getTransactions(){
-        return transactions; //returns the arraylist of all transactions
+    // getter for transactions
+    public ArrayList<Transaction> getTransactions() {
+        return transactions; // returns the arraylist of all transactions
     }
 
-    //add money
-    public void addFunds(int month, int day, double amount){
-        balance = balance + amount; //adding the amount the user wants 
-        transactions.add(new Transaction(month, day, amount)); 
+    // add money
+    public void addFunds(int month, int day, double amount) {
+        balance = balance + amount; // adding the amount the user wants
+        transactions.add(new Transaction(month, day, amount));
     }
 
-    //charge Lunch 
-    public void chargeLunch (int month, int day){
-        balance = balance - 7.0; //lunch costs seven dollars
+    // charge Lunch
+    public void chargeLunch(int month, int day) {
+        balance = balance - 7.0; // lunch costs seven dollars
         transactions.add(new Transaction(month, day, -7.0));
     }
 
-    public String toString(){
+    public String toString() {
         String s = "Name: " + name + " ID: " + idNo + " Balance: $" + balance;
-        s += "Transactions: "; //adding transactions 
-        for (Transaction t: transactions){ // looping through the transactions
-                s += t; // adding the transactions
-            }    
+        s += " Transactions: "; // adding transactions
+        for (Transaction t : transactions) { // looping through the transactions
+            s += t; // adding the transactions
+        }
         return s;
-    } 
+    }
 }
--- Tester.java
+++ Tester.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+
 public class Tester {
     public static void main(String[] args){
+        System.out.println("Welcome to the MealPay program!\r\n" + //
+                        "\r\n" + //
+                        "Adding students to the program...");
+        
+        PaySystem paySystem = new PaySystem();
+        paySystem.addStudent("A");
+        paySystem.addStudent("B");
+        paySystem.addStudent("C");
+        paySystem.addStudent("D");
+
+        System.out.println(paySystem.toString());
     }
 }
