13a284c00e621aecf9f2a531f20d13066b381160
Mira Mavis <mandmvoice@gmail.com>
2026-02-11 22:38:09 -0800

    finished the addFunds method

--- PaySystem.java
+++ PaySystem.java
@@ -8,7 +8,14 @@ public class PaySystem {
         students.add(new Student(name));
     }
 
-    public void addFunds(int idNo, double amount, String date) {
+    public void addFunds(int idNo, int month, int day, double amount) {
+        for (int i = 0; i < students.size(); i++){
+            Student s = students.get(i); //i value where i is looping through
+            if (s.getIdNo() == idNo){ //if id searching for is in the students list
+                s.addFunds(month, day, amount); //new transaction
+            }
+        }
+        System.out.println("Student ID number not found."); //if s.getIdNo does not match with the searching id there is no profile
 
     }
 
